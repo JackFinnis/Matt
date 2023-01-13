d72fc286318a93ab6be83cf5e8f9bfda1b2cf20f
Jack Finnis <jack.finnis@icloud.com>
2023-01-13 21:17:43 +0000

    Initial Commit

---
+++ Matt/MattApp.swift
@@ -0,0 +1,17 @@
+//
+//  MattApp.swift
+//  Matt
+//
+//  Created by Jack Finnis on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MattApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
